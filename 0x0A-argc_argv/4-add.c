e81b6c04106fc30467a237b216555db522b5980f
Ayegbede Joshua <ayegbedejoshua@gmail.com>
2023-09-01 18:55:45 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,70 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
 
 /**
- * main - entry point
- *@argc : arg count
- *@argv : arg vector
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
  *
- *Description: program to print addition of numbers
- *
- *Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
-{
-	int sum;
-	int i = 1;
 
-	while (i < argc)
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
-		i++;
+
+		count++;
 	}
-	printf("%d\n", sum);
+
+	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
